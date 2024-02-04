b4cb9154038aa4e36e2463bfdeefdac954958964
Sanjar <147529957+SanjarPARSIFAL@users.noreply.github.com>
2024-02-04 17:00:12 +0500

    Update Main.java
    ok

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Salom");
-
-        System.out.println("salom1");
-
+        System.out.println("Hello world!");
     }
 }
