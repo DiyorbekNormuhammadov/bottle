9c96b365c91f179b673981f876d117f18c036bfe
Sanjar <147529957+SanjarPARSIFAL@users.noreply.github.com>
2024-02-04 18:05:51 +0500

    Update Main.java
    ok

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        bottle bottle = new bottle();
+        Scanner scanner = new Scanner(System.in);
+        stopF:
+        while (true){
+            System.out.println("""
+                    
+                    1 - suv quyish
+                    2 - suv to'kish
+                    
+                    0 - chiqish
+                    
+                    """);
+            switch (scanner.nextInt()){
+                case 1 -> {
+                    bottle.addWater();
+                }
+                case 2 -> {
+
+                }
+                case 0 ->{
+                    break stopF;
+                }
+            }
+        }
     }
 }
